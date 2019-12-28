1a737be8e343265aac171b51b6d709096f5d28c1
Chief <luyolo.nqobile@gmail.com>
2019-12-28 16:34:56 +0200

    FB15k Data Visualisation
    Visualise entity, relation and fact data as histograms.

--- fixtures/FB15k/view_fact.sql
+++ fixtures/FB15k/view_fact.sql
@@ -5,26 +5,26 @@ drop view if exists fact_test;
 create view fact_train (id, subject, s_name, predicate, object, o_name) as
 select S.id, S.subject, S.name, S.predicate, O.object, O.name
 from (select *
-from train join entity
+from train left join entity
 on subject = synset_id) S, (select *
-from train join entity
+from train left join entity
 on object = synset_id) O
 where S.id = O.id;
 
 create view fact_valid (id, subject, s_name, predicate, object, o_name) as
 select S.id, S.subject, S.name, S.predicate, O.object, O.name
 from (select *
-from valid join entity
+from valid left join entity
 on subject = synset_id) S, (select *
-from valid join entity
+from valid left join entity
 on object = synset_id) O
 where S.id = O.id;
 
 create view fact_test (id, subject, s_name, predicate, object, o_name) as
 select S.id, S.subject, S.name, S.predicate, O.object, O.name
 from (select *
-from test join entity
+from test left join entity
 on subject = synset_id) S, (select *
-from test join entity
+from test left join entity
 on object = synset_id) O
 where S.id = O.id;
